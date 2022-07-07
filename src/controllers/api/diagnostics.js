b2fed9c7763823316d2ce8c67fc30c2652e7f11e
Rami Elsayed <ramielsayed123@outlook.com>
2022-07-07 13:32:15 +0100

    diagnostic route

---
+++ src/controllers/api/diagnostics.js
@@ -0,0 +1,9 @@
+const getDiagnostics = () => {
+
+}
+const postDiagnostics = () => {
+
+}
+
+
+module.exports = {}
--- src/routes/api/diagnostics.js
+++ src/routes/api/diagnostics.js
@@ -1,7 +1,9 @@
 const { Router } = require("express");
 
+const { getDiagnostics, postDiagnostics } = require('../../controllers/api/diagnostics.js')
 const router = Router();
 
-router.get('/')
+router.get('/', getDiagnostics);
+router.post('/', postDiagnostics);
 
 module.exports = router;
